d325ac0093a6d157e7a292db5c32aa0e6df67d91
Johann Malm <cjcmalm@icloud.com>
2019-05-31 11:46:11 +0200

    Update Access.php

--- library/Access.php
+++ library/Access.php
@@ -2,22 +2,26 @@
 
 function Access($roles)
 {
-  if (!is_array(GetHeader('token'))) {
-    $token = GetHeader('token');
+  if (count($roles) > 0) {
+    if (!is_array(GetHeader('token'))) {
+      $token = GetHeader('token');
 
-    $dbh = new DatabaseHelper();
+      $dbh = new DatabaseHelper();
 
-    if ($dbh->doesTableExists('user')) {
-      $user = $dbh->selectByExactCondition('user', ['token' => $token]);
+      if ($dbh->doesTableExists('user')) {
+        $user = $dbh->selectByExactCondition('user', ['token' => $token]);
 
-      if (isset($user[0])) {
-        return in_array($user[0]['role'], $roles);
+        if (isset($user[0])) {
+          return in_array($user[0]['role'], $roles);
+        }
       }
+
     }
 
+    return false;
+  } else {
+    return true;
   }
-
-  return false;
 }
 
 ?>
